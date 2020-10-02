67325b73fe996c612dd3b3780aaef1fca6fe7577
Josh Hug <hug@cs.berkeley.edu>
2020-10-02 11:43:47 -0700

    Added double larger

--- lec/lec1/LargerDemo.java
+++ lec/lec1/LargerDemo.java
@@ -10,6 +10,13 @@ public class LargerDemo {
 	public static void main(String[] args) {
 		System.out.println(larger(-5, 10));
 	}
+
+	public static double larger(double x, double y) {
+		if (x > y) {
+			return x;
+		}
+		return y;
+	}
 } 
 
 /*
