8c0c2bbf5f76273cc0d212fdfe119de18d3a0373
Josh Hug <hug@cs.berkeley.edu>
2020-10-02 11:44:44 -0700

    fix order of methods

--- lec/lec1/LargerDemo.java
+++ lec/lec1/LargerDemo.java
@@ -7,15 +7,15 @@ public class LargerDemo {
 		return y;
 	}
 
-	public static void main(String[] args) {
-		System.out.println(larger(-5, 10));
-	}
-
 	public static double larger(double x, double y) {
 		if (x > y) {
 			return x;
 		}
 		return y;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(larger(-5, 10));
 	}
 } 
 
